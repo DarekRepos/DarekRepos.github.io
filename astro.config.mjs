22355a07f19492e4f04718550067a48aa2bc6a2e
Darek <dareczek06@gmail.com>
2026-01-31 15:52:14 +0100

    fix: ensure correct base path for scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://DarekRepos.github.io',
-    base: '',
+    site: 'https://DarekRepos.github.io/',
     integrations: [
         icon(),
     ],
